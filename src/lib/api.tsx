0d3b56006c9db5128398ba9a60e5794de6f216c5
e-pedidos <epedidosapp2023@gmail.com>
2023-12-13 10:46:32 -0300

    chore: variavel de ambiente next add

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://epedidosapp.info",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
   headers: {
     "content-Type": "application/json",
