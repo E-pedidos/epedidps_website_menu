91a9b2af7c089be44afd92b6e6b3c84daff9f15c
e-pedidos <epedidosapp2023@gmail.com>
2023-12-14 11:44:02 -0300

    chore: add em payload nome da filial

--- src/hook/useMenuFilial.ts
+++ src/hook/useMenuFilial.ts
@@ -3,52 +3,56 @@ import { ICard, IFoodCategory } from "@/types";
 import { useState } from "react";
 
 export const useMenuFilial = () => {
-  const [foodCategorys, setFoodCategorys] = useState<IFoodCategory[]>([]);
-  const [itemsTrending, setItemsTrending] = useState<ICard[]>([]);
-  const [avatarUrl, setAvatarUrl] = useState<string>("");
+  const [foodCategorys, setFoodCategorys] = useState<IFoodCategory[]>([])
+  const [itemsTrending, setItemsTrending] = useState<ICard[]>([])
+  const [nameFilial, setNameFilial] = useState<string>("")
+  const [avatarUrl, setAvatarUrl] = useState<string>("")
 
   const getDataFilial = async (id: string) => {
     try {
       const config = {
         transformResponse: [
           function (data: any) {
-            const dataResponse = JSON.parse(data);
+            const dataResponse = JSON.parse(data)
 
             const payload = {
               foodCategorys: dataResponse.filial.foodCategorys,
               avatarUrl: dataResponse.filial.franchise.user.avatar_url,
-            };
+              name: dataResponse.filial.name
+            }
 
-            const itemsIsTrending = dataResponse.itemsTrending;
+            const itemsIsTrending = dataResponse.itemsTrending
 
             return {
               payload,
               itemsIsTrending,
-            };
+            }
           },
         ],
-      };
+      }
 
       const { data } = await api.get(
         `/filials/getFilialByQrCode/${id}`,
         config
-      );
+      )
 
-      const foodCategorysResponse: IFoodCategory[] = data.payload.foodCategorys;
-      const isTrendingResponse: ICard[] = data.itemsTrending
+      const foodCategorysResponse: IFoodCategory[] = data.payload.foodCategorys
+      const isTrendingResponse: ICard[] = data.itemsIsTrending
 
-      setFoodCategorys(foodCategorysResponse);
-      setAvatarUrl(data.payload.avatar_url);
+      setFoodCategorys(foodCategorysResponse)
+      setAvatarUrl(data.payload.avatar_url)
+      setNameFilial(data.payload.name)
       setItemsTrending(isTrendingResponse)
     } catch (error) {
       console.error(error);
     }
-  };
+  }
 
   return {
     getDataFilial,
     foodCategorys,
     avatarUrl,
-    itemsTrending
-  };
-};
+    itemsTrending,
+    nameFilial
+  }
+}
