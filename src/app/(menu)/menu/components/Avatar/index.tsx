ab8adb0f0583cefd66b413e6b07d9d91c82dae08
e-pedidos <epedidosapp2023@gmail.com>
2023-12-15 11:21:51 -0300

    chore: ajuste no seo do avatar

--- src/app/(menu)/menu/components/Avatar/index.tsx
+++ src/app/(menu)/menu/components/Avatar/index.tsx
@@ -1,13 +1,12 @@
-export const Avatar = ({ image }: { image: string }) => {
-  const containerStyle = {
-    backgroundImage: `url('${image}')`,
-    backgroundSize: "cover",
-  }
 
-  return (
-    <div
-      className="flex items-center justify-center rounded-full h-20 w-20 border-blue-500 border-4"
-      style={containerStyle}
-    />
-  )
+export const Avatar = ({ image }: { image: string }) => {
+  if(image){
+    return (
+      <img
+        alt="logo"
+        src={image}
+        className="flex items-center justify-center rounded-full h-20 w-20 border-blue-500 border-4"
+      />
+    )
+  }
 }
