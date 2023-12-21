51c23d88c11cb38b192affd74ffecbba0509b1cb
e-pedidos <epedidosapp2023@gmail.com>
2023-12-21 20:36:59 -0300

    chore: enviando pedido

--- src/app/(menu)/menu/orders/components/Form/index.tsx
+++ src/app/(menu)/menu/orders/components/Form/index.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { useState } from "react";
-import { Modal } from "../../../../../components/Modal";
 import { useMenuContext } from "@/store/context/menuStore";
-import { IOrder } from "@/types";
 import { getItem } from "@/store/utils/localStorageUtils";
+import { useEffect, useState } from "react";
+import { Modal } from "../../../../../components/Modal";
+import { useWebSocket } from "@/store/hook/useWeSocket";
+import { IOrder } from "@/types";
+import { api } from "@/lib/api";
 
 interface ISelecoes {
   dinheiro: boolean;
@@ -11,10 +13,10 @@ interface ISelecoes {
   pix: boolean;
 }
 
-interface IFormOrder{
-  client_name: string
-    observation: string
-    table_number: string
+interface IFormOrder {
+  client_name: string;
+  observation: string;
+  table_number: number;
 }
 
 export const FormsOrder = () => {
@@ -27,11 +29,13 @@ export const FormsOrder = () => {
     pix: false,
   });
   const [formOrder, setFormOrder] = useState<IFormOrder>({
-    client_name: '',
-    observation: '',
-    table_number: '',
-  })
-  const {listItems, totalOrder} = useMenuContext()
+    client_name: "",
+    observation: "",
+    table_number: 0,
+  });
+  const { listItems, totalOrder } = useMenuContext();
+  const { filialConnectWebSocket, createOrderWebSocket, socket } =
+    useWebSocket();
 
   const openModalBartender = () => {
     setIsModalOpenBartender(true);
@@ -53,6 +57,17 @@ export const FormsOrder = () => {
   const closeModalOrder = () => {
     setIsModalOpenOrder(false);
   };
+  const getOrder = async () => {
+    try {
+      const idFilial = getItem("idFilial");
+
+      const res = await api.get('/orders', { params: { 'filialId': idFilial } });
+
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleSelecaoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { id, checked } = event.target;
@@ -62,29 +77,42 @@ export const FormsOrder = () => {
     }));
   };
 
-  const handleSubmitOrder = (e: React.FormEvent) =>{
+  const handleSubmitOrder = (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const idFilial = getItem('idFilial')
-      const objFormOrder = {
+      const idFilial = getItem("idFilial");
+
+      filialConnectWebSocket(idFilial!);
+
+      const objFormOrder: IOrder = {
         ...formOrder,
-        filialId: idFilial,
-        total_valor: totalOrder,
-        actual_status: 'open',
+        filialId: idFilial!,
+        total_valor: Number(totalOrder),
+        actual_status: "open",
         items: [
-          ...listItems.map(item => ({
+          ...listItems.map((item) => ({
             name: item.nameItemOrder,
-            valor: item.valueItemOrder * item.quantityItemOrder,
-            quantity: item.quantityItemOrder,
-          }))
-        ]
-      }
-      console.log(objFormOrder)
-      closeModalOrder()
+            valor: Number(item.valueItemOrder * item.quantityItemOrder),
+            quantity: Number(item.quantityItemOrder),
+          })),
+        ],
+      };
+
+      createOrderWebSocket(objFormOrder, idFilial!);
+
+      socket!.on("new-order-added", (order) => {
+        console.log("pedido do socket");
+        console.log(order);
+      });
+      closeModalOrder();
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
+
+  useEffect(() => {
+    getOrder();
+  }, []);
 
   if (!isForm) {
     return (
@@ -100,30 +128,45 @@ export const FormsOrder = () => {
               className=" border-blue-500 border rounded-lg mt-2 p-1"
               placeholder="Nome"
               value={formOrder.client_name}
-              onChange={(e)=> setFormOrder({...formOrder, client_name: e.target.value})}
+              onChange={(e) =>
+                setFormOrder({ ...formOrder, client_name: e.target.value })
+              }
             />
             <label>Sua mesa possui idêntificação?</label>
             <input
               className=" border-blue-500 border rounded-lg mt-2 p-1"
               placeholder="Ex: 1"
               value={formOrder.table_number}
-              onChange={(e)=> setFormOrder({...formOrder, table_number: e.target.value})}
+              onChange={(e) =>
+                setFormOrder({
+                  ...formOrder,
+                  table_number: Number(e.target.value),
+                })
+              }
             />
             <h3>tem alguma observação?</h3>
             <input
               className=" border-blue-500 border rounded-lg mt-2 p-1"
               placeholder="Ex: hamburguer sem salada"
               value={formOrder.observation}
-              onChange={(e)=> setFormOrder({...formOrder, observation: e.target.value})}
+              onChange={(e) =>
+                setFormOrder({ ...formOrder, observation: e.target.value })
+              }
             />
-            <button className="mt-4 bg-green-500 text-white p-2 rounded ml-6" type="submit">
+            <button
+              className="mt-4 bg-green-500 text-white p-2 rounded ml-6"
+              type="submit"
+            >
               Enviar
             </button>
           </form>
         </Modal>
         <div className="flex items-center gap-4">
           <h3>Deseja enviar seu pedido?</h3>
-          <button className="bg-blue-500 p-1 rounded-2xl text-white font-medium my-2" onClick={openModalOrder}>
+          <button
+            className="bg-blue-500 p-1 rounded-2xl text-white font-medium my-2"
+            onClick={openModalOrder}
+          >
             Enviar
           </button>
         </div>
--- src/store/hook/useWeSocket.ts
+++ src/store/hook/useWeSocket.ts
@@ -1,23 +1,46 @@
+import { IOrder } from "@/types";
 import { useEffect, useState } from "react";
 import { Socket, io } from "socket.io-client";
 
 export const useWebSocket = () => {
-  const [socket, setSocket] = useState<Socket | null>(null);
+  const [socket, setSocket] = useState<undefined | Socket>(undefined);
+
+  const filialConnectWebSocket = (filial: string) => {
+    try {
+      socket!.emit("enter-filial", filial, (message: any) => {
+        console.log(message);
+      });
+    } catch (error) {
+      console.error("Erro interno");
+    }
+  };
+
+  const createOrderWebSocket = (order: IOrder, filial: string) => {
+    try {
+      socket!.emit("create-order", order, filial, () => {
+        console.log("pedido envidado");
+      });
+    } catch (error) {
+      console.error("Erro interno");
+    }
+  };
 
   useEffect(() => {
     try {
-        const newSocket = io(process.env.NEXT_PUBLIC_API_URL!);
-        setSocket(newSocket);
+      const newSocket = io(process.env.NEXT_PUBLIC_API_URL!);
+      setSocket(newSocket);
 
-        return () => {
-          newSocket.disconnect();
-        };
-      } catch (error) {
-        console.error("Erro ao criar o WebSocket:", error);
-      }
+      return () => {
+        newSocket.disconnect();
+      };
+    } catch (error) {
+      console.error("Erro ao criar o WebSocket:", error);
+    }
   }, []);
 
   return {
     socket,
+    filialConnectWebSocket,
+    createOrderWebSocket,
   };
 };
