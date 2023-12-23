101477c2f06b6a2a63bd04eb8033c99d61a125eb
e-pedidos <epedidosapp2023@gmail.com>
2023-12-23 11:46:59 -0300

    chore: cirado util para obj localStorage

--- src/store/utils/localStorageUtils.tsx
+++ src/store/utils/localStorageUtils.tsx
@@ -6,6 +6,16 @@ export const getItem = (key: string) => {
     return localStorage.getItem(key)
 }
 
+export const setItemObject = (key: string, item: object) => {
+    const jsonItem = JSON.stringify(item)
+    return localStorage.setItem(key, jsonItem)
+}
+
+export const getItemObject = (key: string) => {
+    const jsonItem = localStorage.getItem(key)
+    return JSON.parse(jsonItem!)
+}
+
 export const remove = (key: string) => {
     return localStorage.removeItem(key)
 }
